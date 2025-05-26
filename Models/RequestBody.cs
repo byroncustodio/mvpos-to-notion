fc68ed254b0f2d6562af5dcbf07b67986a5bc62b
Byron Custodio <byron.custodio@gmail.com>
2025-05-25 22:56:18 -0700

    fix: adding json annotations to request body

--- Models/RequestBody.cs
+++ Models/RequestBody.cs
@@ -1,18 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace mvpos.Models;
 
 public class RequestBody
 {
+    [JsonProperty("email")]
     private string Email { get; set; }
+    
+    [JsonProperty("password")]
     private string Password { get; set; }
+    
+    [JsonProperty("uploadType")]
     private string UploadType { get; set; }
+    
+    [JsonProperty("notionPageId")]
     private string NotionPageId { get; set; }
 
-    private DateTime FromDate { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(-1);
-    private DateTime ToDate { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddSeconds(-1);
-    private int Limit { get; set; }
-    private List<string> Vendors { get; set; } = [];
-    private bool Debug { get; set; }
+    [JsonProperty("fromDate")]
+    private DateTime? FromDate { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(-1);
+    
+    [JsonProperty("toDate")]
+    private DateTime? ToDate { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddSeconds(-1);
+    
+    [JsonProperty("limit")]
+    private int? Limit { get; set; }
+
+    [JsonProperty("debug")]
+    private int Debug { get; set; }
 }
